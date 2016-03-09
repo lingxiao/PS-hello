13232e3439add981315375a0c5fa23575a0d74f0
Xiao Ling <lingxiao@seas.upenn.edu>
2016-03-09 10:55:24 -0500

    1055

--- src/Main.purs
+++ src/Main.purs
@@ -7,7 +7,17 @@
 -- | Source : https://github.com/paf31/purescript-thermite
 -- |          http://www.parsonsmatt.org/2015/10/03/elm_vs_purescript.html
 -- |          https://github.com/ethul/purescript-react-example     <- project structure
--- |          pulp browserify --to app.js
+-- | Workflow: 
+-- |          pulp init   - create project folder, with bower.json
+-- |          npm  init   - create package.json
+-- |          bower install --save [PACKAGE] to note dependencies in bower.json
+-- |          npm   install --save [PACKAGE] to note dependences in package.json
+-- |          Now build app.js file,  instead of pulp browserfy, do:
+-- |              rm -r output
+-- |              pulp build --to browserify-entry-point.js
+-- |              browserify browserify-entry-point.js > app.js
+-- |          Finally run development server. But instead of pulp server do: 
+-- |              serve (installed by  npm install -g serve)
 -- | 
 ---------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------
@@ -40,7 +50,6 @@ import Data.Nullable (toMaybe)
 -- * https://github.com/paf31/try-thermite/blob/gh-pages/staging/src/Thermite/Try.purs
 ---------------------------------------------------------------------------------------------------
 
-
 -- * note :   void :: forall f a. Functor f => f a -> f ()
 defaultMain :: forall state action eff. 
                       T.Spec _ state _ action -> state -> Eff (dom :: DOM.DOM | eff) Unit
@@ -66,13 +75,33 @@ state0 = 0
 render :: T.Render State _ _ 
 render _ _ state _ = [ R.h1' [ R.text "Hello world hello "]]
 
+
 spec :: T.Spec _ State _ _ 
 spec = T.simpleSpec T.defaultPerformAction render
-
 
 -- * main: just displays the state
 main :: forall m. Eff (dom :: DOM.DOM | m ) Unit
 main = defaultMain spec state0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
